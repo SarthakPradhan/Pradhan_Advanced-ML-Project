5245bfcff04ea0c761f3594f6e04b3a4be6ae447
SarthakPradhan <40028495+SarthakPradhan@users.noreply.github.com>
2021-10-28 11:07:13 -0400

    Update Pradhan_ML_Project_Fall2019.py

--- Pradhan_ML_Project_Fall2019.py
+++ Pradhan_ML_Project_Fall2019.py
@@ -20,7 +20,7 @@ import matplotlib.pyplot as plt
 image_size = 28 # width and length
 no_of_different_labels = 10 #  i.e. 0, 1, 2, 3, ..., 9
 image_pixels = image_size * image_size
-data_path = "C:/Users/ASUS/Desktop/Virginia TEch/Advanced ML/project/"
+data_path = "C:/Users/ASUS/Desktop/Virginia TEch/Advanced ML/project/" #replace with your own directory where handwritten digit dataset is stored
 train_data = np.loadtxt(data_path + "mnist_train.csv", 
                         delimiter=",")
 test_data = np.loadtxt(data_path + "mnist_test.csv", 
